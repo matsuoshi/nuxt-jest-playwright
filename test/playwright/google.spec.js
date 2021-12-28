06e7a5edb6ff3e9cbc1cd31113413d01b5d3dc13
matsuoshi <matsuoshi@gmail.com>
2021-12-29 07:24:36 +0900

    スクリーンショット設定 (#1)
    * スクリーンショット設定
    
    * E2Eテスト google検索後のクリック→ページ遷移確認を追加

--- test/playwright/google.spec.js
+++ test/playwright/google.spec.js
@@ -10,7 +10,12 @@ test('search TAM on Google', async ({ page }) => {
     page.waitForNavigation(),
     page.press('[name=q]', 'Enter')
   ])
-  await page.screenshot({ path: 'screenshots/google-result.png', fullPage: true });
-
   await expect(page.locator('#main')).toContainText('tam-tam.co.jp')
+
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.click('text=tam-tam.co.jp')
+  ])
+  expect(page.url()).toContain('www.tam-tam.co.jp')
+  await expect(page).toHaveTitle(/TAM｜クリエイティブテック・エージェンシー/)
 })
